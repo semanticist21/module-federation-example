bc8cad170eb9c823af36d93a61e07a0fbbbb89dc
semanticist21 <semanticist0@gmail.com>
2024-07-23 17:55:59 +0900

    🎨

--- src/host/src/store/store.ts
+++ src/host/src/store/store.ts
@@ -1,11 +1,19 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface IncrementStore {
   count: number;
   increment: () => void;
 }
 
-export const useIncrementStore = create<IncrementStore>((set) => ({
-  count: 0,
-  increment: () => set((state) => ({ count: state.count + 1 })),
-}));
+export const useIncrementStore = create(
+  persist<IncrementStore>(
+    (set) => ({
+      count: 0,
+      increment: () => set((state) => ({ count: state.count + 1 })),
+    }),
+    {
+      name: "increment-store",
+    }
+  )
+);
--- src/remote/@mf-types/hostApp/compiled-types/Store.d.ts
+++ src/remote/@mf-types/hostApp/compiled-types/Store.d.ts
@@ -2,5 +2,15 @@ interface IncrementStore {
     count: number;
     increment: () => void;
 }
-export declare const useIncrementStore: import("zustand").UseBoundStore<import("zustand").StoreApi<IncrementStore>>;
+export declare const useIncrementStore: import("zustand").UseBoundStore<Omit<import("zustand").StoreApi<IncrementStore>, "persist"> & {
+    persist: {
+        setOptions: (options: Partial<import("zustand/middleware").PersistOptions<IncrementStore, IncrementStore>>) => void;
+        clearStorage: () => void;
+        rehydrate: () => Promise<void> | void;
+        hasHydrated: () => boolean;
+        onHydrate: (fn: (state: IncrementStore) => void) => () => void;
+        onFinishHydration: (fn: (state: IncrementStore) => void) => () => void;
+        getOptions: () => Partial<import("zustand/middleware").PersistOptions<IncrementStore, IncrementStore>>;
+    };
+}>;
 export {};
